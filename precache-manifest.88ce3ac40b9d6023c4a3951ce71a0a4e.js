c5e3f2e47786f54b3c9f0ea0da2e6a31cf3a0eb5
Anonymous <xolpolatovshohjahhon5@gmail.com>
2021-07-27 00:31:24 +0500

    Updates

---
+++ precache-manifest.88ce3ac40b9d6023c4a3951ce71a0a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecb23e33cff7380cc6262648000a963",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "ed6d3f9e50167ac3a83d",
+    "url": "/react-calculator/static/css/main.8f2410ae.chunk.css"
+  },
+  {
+    "revision": "4efdd5f72145c1cca6b2",
+    "url": "/react-calculator/static/js/2.50244f75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.50244f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6d3f9e50167ac3a83d",
+    "url": "/react-calculator/static/js/main.9df2444b.chunk.js"
+  },
+  {
+    "revision": "cbb41c4b3088b1b89848",
+    "url": "/react-calculator/static/js/runtime-main.c495d3f6.js"
+  }
+]);
